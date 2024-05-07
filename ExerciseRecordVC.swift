5e1143cb3a8b3654af05f0bb36ca2ff560a43e82
시모니(Simoni) <145527459+simoni-git@users.noreply.github.com>
2024-05-07 18:16:39 +0900

    셀에 추가,삭제 버튼 클릭시 스택뷰 추가,삭제 구현
    셀에 추가,삭제 버튼 클릭시 스택뷰 추가,삭제 구현
    셀에 초기에 있던 텍스트필드는 그냥 히든처리 해놓고, 스택뷰배열을 통하여 나타나게해서 순서정보를 통해 나중에 데이터를 처리하려고 이렇게 구현함.

--- ExerciseRecordVC.swift
+++ ExerciseRecordVC.swift
@@ -23,6 +23,7 @@ class ExerciseRecordVC: UIViewController {
         self.tableView.delegate = self
         configure()
       
+      
     }
     
     func configure() {
@@ -35,14 +36,19 @@ class ExerciseRecordVC: UIViewController {
 extension ExerciseRecordVC: UITableViewDataSource , UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? Cell else {
             return UITableViewCell()
         }
+       
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension // 셀높이는 오토로 유동적으로!
     }
     
 }
@@ -54,14 +60,22 @@ class Cell: UITableViewCell {
     @IBOutlet weak var volumeLabel: UILabel!
     
     @IBOutlet weak var verticalStackView: UIStackView!
+    @IBOutlet weak var horizontalStackView: UIStackView!
+    var horizontalStackViews: [UIStackView] = [] // 수평 스택 뷰를 저장할 배열
+    
+    
+    @IBOutlet weak var verticalStackViewHeightConst: NSLayoutConstraint!
+    
     
     @IBOutlet weak var setLabel: UILabel!
     @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var kgLabel: UILabel!
     @IBOutlet weak var countTextField: UITextField!
+    @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var checkBtn: UIButton!
     
-    
     @IBAction func tapCheckBtn(_ sender: UIButton) {
+        
     }
     
     
@@ -70,12 +84,78 @@ class Cell: UITableViewCell {
     
     
     @IBAction func tapDeleteSetBtn(_ sender: UIButton) {
+        deleteLatestHorizontalStackView()
+        reloadCell()
     }
     
     
     @IBAction func tapAddSetBtn(_ sender: UIButton) {
+        addNewHorizentalStackView()
+        reloadCell()
     }
     
+    
+    private func addNewHorizentalStackView() {
+        let horizontalStackView = UIStackView()
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.alignment = .fill
+        horizontalStackView.distribution = .fillEqually
+        horizontalStackView.spacing = 0
+        horizontalStackView.backgroundColor = .systemPink
+        
+        let setLabel = UILabel()
+        setLabel.text = "\(horizontalStackViews.endIndex + 1) 세트"
+        
+        let weightTextField = UITextField()
+        weightTextField.placeholder = "0"
+        weightTextField.backgroundColor = .white
+        weightTextField.layer.cornerRadius = 5
+        
+        let weightLabel = UILabel()
+        weightLabel.text = "KG"
+        
+        let countTextField = UITextField()
+        countTextField.placeholder = "0"
+        countTextField.backgroundColor = .white
+        countTextField.layer.cornerRadius = 5
+        
+        let countLabel = UILabel()
+        countLabel.text = "회"
+        
+        let checkBtn = UIButton(type: .custom)
+        checkBtn.setImage( .checkmark , for: .normal)
+        checkBtn.backgroundColor = .systemGray
+        
+        horizontalStackView.addArrangedSubview(setLabel)
+        horizontalStackView.addArrangedSubview(weightTextField)
+        horizontalStackView.addArrangedSubview(weightLabel)
+        horizontalStackView.addArrangedSubview(countTextField)
+        horizontalStackView.addArrangedSubview(countLabel)
+        horizontalStackView.addArrangedSubview(checkBtn)
+        
+       
+        verticalStackViewHeightConst.constant += 20
+        verticalStackView.addArrangedSubview(horizontalStackView)
+        horizontalStackViews.append(horizontalStackView) // 배열에 추가
+        
+    }
+    
+    private func deleteLatestHorizontalStackView() {
+           guard let lastView = horizontalStackViews.popLast() else { return }
+           
+        verticalStackViewHeightConst.constant -= 20
+           verticalStackView.removeArrangedSubview(lastView)
+           lastView.removeFromSuperview()
+     
+       }
+    
+    private func reloadCell() {
+           guard let tableView = superview as? UITableView else { return }
+           tableView.beginUpdates()
+           tableView.endUpdates()
+       }
+    
+   
     
 }
 
